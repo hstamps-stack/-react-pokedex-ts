a68bfba3fcff34c0f47f4d712dbd810f83d67513
Harrsion <hstamps4@gmail.com>
2021-04-15 23:35:34 -0700

    feat(Navbar): used react-router-dom to add navigation functionality

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark text-white'>
+      <Link className='navbar-brand' to='/'>
+        React Pokemon
+      </Link>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNavAltMarkup'
+        aria-controls='navbarNavAltMarkup'
+        aria-expanded='false'
+        aria-label='Toggle navigation'>
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+        <div className='navbar-nav'>
+          <Link className='nav-link' to='/'>
+            Home
+          </Link>
+
+          <Link className='nav-link' to='/pokemon'>
+            Pokemon
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
